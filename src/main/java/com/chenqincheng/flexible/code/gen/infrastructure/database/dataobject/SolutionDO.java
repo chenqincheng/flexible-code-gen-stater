fd2502d9e42bff3eda9f5631090964405f4df4cf
chengqincheng <191318455@qq.com>
2023-07-12 23:33:21 +0800

    update: 提交生存方案类

---
+++ src/main/java/com/chenqincheng/flexible/code/gen/infrastructure/database/dataobject/SolutionDO.java
@@ -0,0 +1,64 @@
+package com.chenqincheng.flexible.code.gen.infrastructure.database.dataobject;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 域
+ */
+@Data
+@TableName(value = "solution")
+public class SolutionDO {
+
+    @TableId
+    private Long id;
+
+    /**
+     * 方案id
+     */
+    @TableField(value = "solution_id")
+    private Long solutionId;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+
+    /**
+     * 别名
+     */
+    private String alias;
+
+
+    /**
+     * 模块编码
+     */
+    private String code;
+
+    /**
+     * 类型
+     */
+    private String type;
+
+
+    @TableLogic
+    private Integer is_deleted;
+
+    @TableField(value = "created_at")
+    private Date createdAt;
+
+    @TableField(value = "created_by")
+    private Long createdBy;
+
+    @TableField(value = "updated_at")
+    private Date updatedAt;
+
+    @TableField(value = "updated_by")
+    private Long updatedBy;
+}
---
+++ src/main/java/com/chenqincheng/flexible/code/gen/infrastructure/database/dataobject/SolutionItemDO.java
@@ -0,0 +1,59 @@
+package com.chenqincheng.flexible.code.gen.infrastructure.database.dataobject;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 域
+ */
+@Data
+@TableName(value = "solution_item")
+public class SolutionItemDO {
+
+    @TableId
+    private Long id;
+
+    /**
+     * 方案模块id
+     */
+    @TableField(value = "solution_module_id")
+    private Long solutionModuleId;
+
+
+    @TableField(value = "package_id")
+    private Long packageId;
+
+    @TableField(value = "template_id")
+    private Long templateId;
+
+    /**
+     * 类型
+     */
+    private String name;
+
+    /**
+     * 类型
+     */
+    private String type;
+
+
+    @TableLogic
+    private Integer is_deleted;
+
+    @TableField(value = "created_at")
+    private Date createdAt;
+
+    @TableField(value = "created_by")
+    private Long createdBy;
+
+    @TableField(value = "updated_at")
+    private Date updatedAt;
+
+    @TableField(value = "updated_by")
+    private Long updatedBy;
+}
